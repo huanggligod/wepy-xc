384bbd1c32b3df3ab50675fb7739ed1b03369505
huanggang <516812614@qq.com>
2018-04-14 18:14:49 +0800

    12

--- src/util/util.js
+++ src/util/util.js
@@ -8,6 +8,20 @@ function isEmptyObject(e) {
   }    
   return !0
 }
+function getCurrentTime() {
+  var keep = '';
+  var date = new Date();
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  m = m < 10 ? '0' + m : m;
+  var d = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  var h = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+  var f = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+  var s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+  var rand = Math.round(Math.random() * 899 + 100);
+  keep = y + '' + m + '' + d + '' + h + '' + f + '' + s;
+  return keep; //20160614134947
+}
 // 时间戳
 function dateFormat(timestamp, formats) {
   formats = formats || 'Y-m-d'
@@ -75,5 +89,6 @@ function checkSettingStatu() {
 module.exports = {
   isEmptyObject: isEmptyObject,
   checkSettingStatu: checkSettingStatu,
+  getCurrentTime: getCurrentTime,
   dateFormat: dateFormat
 }
